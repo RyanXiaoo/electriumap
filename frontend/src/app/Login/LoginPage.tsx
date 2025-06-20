42b1241e10195deb59cdc97c358dec36ec21b3aa
a33peng <144617347+a33peng@users.noreply.github.com>
2025-06-20 15:57:44 -0400

    Google/Facebook/Apple authentication and create account page   (#32)
    * Implement Google, Facebook, and Apple authentication
    
    * Implement create account page
    
    * fix: create account url

--- frontend/src/app/Login/LoginPage.tsx
+++ frontend/src/app/Login/LoginPage.tsx
@@ -1,6 +1,10 @@
 "use client"; 
 
 import React, {useState} from 'react';
+import Image from 'next/image'; 
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
 // import * as Yup from "yup";
 
 type LoginPageProps = { 
@@ -8,28 +12,42 @@ type LoginPageProps = {
     password: string; 
 }
 
+type AuthButtonProps = { 
+    src: string; 
+    alt: string; 
+    text: string; 
+};
+
+const AuthButton: React.FC<AuthButtonProps> = ({src, alt, text}) => ( 
+    <button className="flex items-center gap-2 justify-center border border-white text-white px-2 py-1 rounded hover:bg-green-700 transition w-full"> 
+        <Image src={src} alt={alt} width={20} height={20} />
+        {text}
+    </button>
+);
+
 const LoginPage: React.FC = () => {
     const [username, setUsername] = useState(''); 
     const [pwd, setPwd] = useState(''); 
+    const router = useRouter()
 
     // pop-up alert for form validation (username and password) 
     const handleLogin = (event: React.FormEvent) => { 
         event.preventDefault(); 
-
         if(!username || !pwd){ 
             alert("Please enter your username and password.")
             return; 
         }
-
         alert('Submitted successfully.')
+        // after user successfully logins, change route to map page 
+        router.push('/');
     };  
 
     return(
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <h1 className="text-2xl font-bold text-white mt-40 mb-1">Sign In Electriumap</h1> 
-            <form onSubmit={handleLogin} className="w-[300px]"> 
+            <h1 className="text-2xl font-bold text-white mt-20 mb-1">Sign In Electriumap</h1> 
+            <form onSubmit={handleLogin} className="w-[400px]"> 
             <div className="flex flex-col gap-3">
-                <label htmlFor="username" className="mb-1 font-medium">Username</label>
+                <label htmlFor="username" className="mb-0 font-medium">Username</label>
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
                 <input
                     id="username"
@@ -38,7 +56,7 @@ const LoginPage: React.FC = () => {
                     onChange={(e) => setUsername(e.target.value)}
                     className="w-full p-2 rounded border border-white bg-black text-white"/>
                 </div>
-                <label htmlFor="password" className="mb-1 font-medium">Password</label>
+                <label htmlFor="password" className="mb-0 font-medium">Password</label>
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
                 <input
                     id="password"
@@ -50,10 +68,23 @@ const LoginPage: React.FC = () => {
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
                 <button 
                     className="w-full bg-white text-black p-2 rounded hover:bg-gray-200 transition"
-                    type="submit">Login</button>
+                    type="submit"
+                    // onClick={() => router.push('/')}
+                    >Login</button>
                 </div>
             </div>
+
+            {/* different open authorization buttons for login*/}
+            <div className="w-[400px] flex flex-col gap-2 mt-10"> 
+                <AuthButton src="/images/google_logo.png" alt="Google" text="Continue with Google" />
+                <AuthButton src="/images/facebook_logo.png" alt="Facebook" text="Continue with Facebook" />
+                <AuthButton src="/images/apple_logo.png" alt="Apple" text="Continue with Apple" />
+            </div> 
+
             </form> 
+            <p className="text-white"> Don't have an account? {" "}
+                <Link href="/create-account">Sign up</Link> 
+            </p>
         </div>
         );  
     }
