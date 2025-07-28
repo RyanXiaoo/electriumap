51b77c2212778d0c930cde1a9fb5f2b495498195
a33peng <144617347+a33peng@users.noreply.github.com>
2025-07-27 23:01:03 -0400

    validate login authentication status (#92)

--- frontend/src/app/globals.ts
+++ frontend/src/app/globals.ts
@@ -1,10 +1,19 @@
 // helper function to save user's first name from profile setup 
 let globalFirstName: string = "";
-
 export function setGlobalFirstName(name: string) {
   globalFirstName = name;
 }
 
 export function getGlobalFirstName() {
   return globalFirstName;
+}
+
+// status flag to check if user is authenticated via Firebase 
+let isAuthenticated: boolean = false; 
+export function setIsAuthenticated(status: boolean){ 
+  return isAuthenticated = status 
+}
+
+export function getIsAuthenticated() { 
+  return isAuthenticated
 }
